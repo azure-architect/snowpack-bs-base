b0fea4d6cee9562b8c179d0b170a0ffa5cf41d4f
azure-architect <vscoder.brad@gmail.com>
2023-10-15 06:49:03 +0800

    this is the skeloton of the project without the bootstrap stuff built in.

---
+++ snowpack.config.js
@@ -0,0 +1,31 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    ['@snowpack/plugin-sass',{
+      'compilerOptions':{
+          style: 'compressed',
+          loadPath: [
+              './scss/main.scss'
+              
+          ]
+      }
+  }],
+
+
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
